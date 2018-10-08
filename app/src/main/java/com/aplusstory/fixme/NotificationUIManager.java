3bf1bb05f9dc61fdb3dcc4232dcdad752b268027
plainswalker <plainswalker615@gmail.com>
2018-10-08 19:39:53 +0900

    noti structture dedign/ change architecture for seperating notification/schedule alarm

--- app/src/main/java/com/aplusstory/fixme/NotificationUIManager.java
+++ app/src/main/java/com/aplusstory/fixme/NotificationUIManager.java
@@ -1,5 +1,32 @@
 package com.aplusstory.fixme;
 
-public interface NotificationUIManager {
-    int setNotifier(Notifier m);
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.widget.Toast;
+
+
+public abstract class NotificationUIManager {
+    private NotificationDataManager fmnm;
+    private NotificationManager nm;
+    private NotificationChannel nch;
+    private Notification ni;
+    private Toast ltmi;
+
+    public int setDataManager(NotificationDataManager m){
+        try {
+            this.fmnm = m;
+        }
+        catch(Exception e){
+            return 1;
+        }
+
+        return 0;
+    }
+
+    abstract public int setNotification(Notification nt);
+
+    abstract public int updateNotification(String msg, long elt);
+
+
 }
