983c6405e54b9820201771106dd841a57473d3ee
aileenpark <aileen.bak1214@gmail.com>
2018-11-02 19:17:22 +0900

    first commit

---
+++ app/src/main/java/com/example/nayunpark/fixme_ui/SplashActivity.java
@@ -0,0 +1,21 @@
+package com.example.nayunpark.fixme_ui;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class SplashActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        try {
+            Thread.sleep(2000);
+        } catch(InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        startActivity(new Intent(this, MainActivity.class));
+        finish();
+    }
+}
