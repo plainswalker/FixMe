908710019f35ff4e45b5aa58bb049276187958f0
kjw0412c <kjw0412c@gmail.com>
2018-11-04 18:19:25 +0900

    CalendarFix
    Fix

---
+++ FixMe/app/src/main/java/com/aplusstory/fixme/cal/WeatherInfo.java
@@ -0,0 +1,16 @@
+package com.aplusstory.fixme.cal;
+/**
+ * Weather Info.<br>
+ *
+ * Created by brownsoo
+ */
+public class WeatherInfo {
+
+    public enum Weather {
+        SUNSHINE,
+        SUN_CLOUDY,
+        CLOUDY,
+        RAINY,
+        SNOW
+    }
+}
