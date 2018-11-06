af6941faee40f37cf2018e3e2b75414d08fd9b48
plainswalker <plainswalker615@gmail.com>
2018-11-07 03:26:15 +0900

    integrated adviser function and setting

--- app/src/main/java/com/aplusstory/fixme/SettingsActivity.java
+++ app/src/main/java/com/aplusstory/fixme/SettingsActivity.java
@@ -2,12 +2,15 @@ package com.aplusstory.fixme;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.CompoundButton;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.RelativeLayout;
@@ -17,10 +20,11 @@ import android.widget.Toast;
 import android.support.v7.widget.Toolbar;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 
-public class SettingsActivity extends AppCompatActivity{
+public class SettingsActivity extends AppCompatActivity {
 
     Toolbar toolbar2;
 
@@ -41,60 +45,47 @@ public class SettingsActivity extends AppCompatActivity{
                 ,this.getString(R.string.setting_item_alert_light_sensivity)
                 ,this.getString(R.string.setting_item_alert_period)
         };
-        ListView list1 = (ListView) findViewById(R.id.settingList1);
 
-        final String[] setlistV = {
-                 this.getString(R.string.setting_item_alert_vibration)
-        };
+        ListView list1 = (ListView) findViewById(R.id.settingList1);
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, setlist1);
+        list1.setAdapter(arrayAdapter);
+        list1.setOnItemClickListener(
+                new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                        switch(position) {
+                            case 0:
+                                Intent intent = new Intent(getApplicationContext(), AlarmOpacityActivity.class);
+
+                                startActivity(intent);
+                                break;
+                            case 1:
+                                Intent intent2 = new Intent(getApplicationContext(), LightSensActivity.class);
+                                startActivity(intent2);
+                                break;
+                            case 2:
+                                Intent intent3 = new Intent(getApplicationContext(), AlarmPeriodActivity.class);
+                                startActivity(intent3);
+                                break;
+                        }
+                    }
+                }
+        );
+
+        final ArrayList<String> setlistV = new ArrayList<String>();
+        setlistV.add(this.getString(R.string.setting_item_alert_vibration));
+        setlistV.add(this.getString(R.string.setting_item_alert_activate));
         ListView listV = (ListView) findViewById(R.id.settingListV);
+        SwitchListviewAdapter listviewAdapter = new SwitchListviewAdapter(this, R.layout.switch_list, setlistV);
+        listV.setAdapter(listviewAdapter);
 
         final String[] setlist2 = {
                  this.getString(R.string.setting_item_footprint_data_duration)
                 ,this.getString(R.string.setting_item_footprint_location_bookmark)
         };
         ListView list2 = (ListView) findViewById(R.id.settingList2);
-
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, setlist1);
-        list1.setAdapter(arrayAdapter);
-
-        list1.setOnItemClickListener(
-            new AdapterView.OnItemClickListener() {
-                @Override
-                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                    switch(position) {
-                        case 0:
-                            Intent intent = new Intent(getApplicationContext(), AlarmOpacityActivity.class);
-
-                            startActivity(intent);
-                            break;
-                        case 1:
-                            Intent intent2 = new Intent(getApplicationContext(), LightSensActivity.class);
-                            startActivity(intent2);
-                            break;
-                        case 2:
-                            Intent intent3 = new Intent(getApplicationContext(), AlarmPeriodActivity.class);
-                            startActivity(intent3);
-                            break;
-                    }
-                }
-            }
-        );
-
-        ArrayAdapter<String> arrayAdapterV = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, setlistV);
-        listV.setAdapter(arrayAdapterV);
-
-        listV.setOnItemClickListener(
-            new AdapterView.OnItemClickListener() {
-                @Override
-                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                    Toast.makeText(getApplicationContext(), setlistV[position],Toast.LENGTH_SHORT).show();
-                }
-            }
-        );
-
         ArrayAdapter<String> arrayAdapter2 = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, setlist2);
         list2.setAdapter(arrayAdapter2);
-
         list2.setOnItemClickListener(
             new AdapterView.OnItemClickListener() {
                 @Override
