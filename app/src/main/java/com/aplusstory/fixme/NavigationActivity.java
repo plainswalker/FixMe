460ce911d38bb3c6ff63bca6c1e23848bc5f3449
plainswalker <plainswalker615@gmail.com>
2018-11-07 23:49:08 +0900

    schedule ui code imported

--- app/src/main/java/com/aplusstory/fixme/NavigationActivity.java
+++ app/src/main/java/com/aplusstory/fixme/NavigationActivity.java
@@ -1,5 +1,6 @@
 package com.aplusstory.fixme;
 
+import android.app.FragmentManager;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -12,6 +13,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class NavigationActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
