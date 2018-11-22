b805622e32e8f340f5cb8b26134bee673016b584
aileenpark <aileen.bak1214@gmail.com>
2018-11-22 19:40:50 +0900

    be able to get/set scheduleColor dynamically

--- app/src/main/java/com/example/nayunpark/fixme_ui/ColorRecyclerAdapter.java
+++ app/src/main/java/com/example/nayunpark/fixme_ui/ColorRecyclerAdapter.java
@@ -1,5 +1,8 @@
 package com.example.nayunpark.fixme_ui;
 
+import android.content.Context;
+import android.graphics.drawable.GradientDrawable;
+import android.graphics.drawable.LayerDrawable;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -11,6 +14,8 @@ import android.widget.TextView;
 import java.util.ArrayList;
 
 public class ColorRecyclerAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+
+    private Context context;
 
     public static class ColorRecyclerViewHolder extends RecyclerView.ViewHolder {
 
@@ -33,6 +38,7 @@ public class ColorRecyclerAdapter extends RecyclerView.Adapter<RecyclerView.View
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
         View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.recycler_view_row, viewGroup, false);
+        context = viewGroup.getContext();
         return new ColorRecyclerViewHolder(view);
     }
 
@@ -40,7 +46,40 @@ public class ColorRecyclerAdapter extends RecyclerView.Adapter<RecyclerView.View
     public void onBindViewHolder(@NonNull RecyclerView.ViewHolder viewHolder, int i) {
         ColorRecyclerViewHolder colorRecyclerViewHolder = (ColorRecyclerViewHolder) viewHolder;
 
+        LayerDrawable imageShape = (LayerDrawable) context.getResources().getDrawable(R.drawable.image_color);
+        GradientDrawable imageColor = (GradientDrawable) imageShape.findDrawableByLayerId(R.id.gradient_drawable);
+
         colorRecyclerViewHolder.colorImageView.setImageResource(settingsColorInfoArrayList.get(i).colorId);
+        switch (i) {
+            default:
+                imageColor.setColor(context.getResources().getColor(R.color.scheduleColor1));
+                break;
+            case 0:
+                imageColor.setColor(context.getResources().getColor(R.color.scheduleColor2));
+                break;
+            case 1:
+                imageColor.setColor(context.getResources().getColor(R.color.scheduleColor3));
+                break;
+            case 2:
+                imageColor.setColor(context.getResources().getColor(R.color.scheduleColor4));
+                break;
+            case 3:
+                imageColor.setColor(context.getResources().getColor(R.color.scheduleColor5));
+                break;
+            case 4:
+                imageColor.setColor(context.getResources().getColor(R.color.scheduleColor6));
+                break;
+            case 5:
+                imageColor.setColor(context.getResources().getColor(R.color.scheduleColor7));
+                break;
+            case 6:
+                imageColor.setColor(context.getResources().getColor(R.color.scheduleColor8));
+                break;
+            case 7:
+                imageColor.setColor(context.getResources().getColor(R.color.scheduleColor9));
+                break;
+        }
+
         colorRecyclerViewHolder.colorTextView.setText(settingsColorInfoArrayList.get(i).colorName);
     }
 
--- app/src/main/java/com/example/nayunpark/fixme_ui/ScheduleColorActivity.java
+++ app/src/main/java/com/example/nayunpark/fixme_ui/ScheduleColorActivity.java
@@ -1,5 +1,6 @@
 package com.example.nayunpark.fixme_ui;
 
+import android.graphics.drawable.GradientDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
